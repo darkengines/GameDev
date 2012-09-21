1c4c8342427b20c17f578f8a9e2acd906eef640c
Quicksort <Quicksort@302f0312-3c5d-0d4b-b714-136c201f073e>
2012-09-21 18:38:26 +0000

    git-svn-id: https://darkengines.com/svn/darkengines/GameDev@250 302f0312-3c5d-0d4b-b714-136c201f073e

---
+++ Engine/Engine/TPlane.h
@@ -0,0 +1,40 @@
+#ifndef _TPLANE_H_
+#define _TPLANE_H_
+
+#include "System.h"
+#include "TVector3.h"
+
+template<class Real>
+class TPlane {
+private:
+public:
+	TPlane(const TVector3<Real>& point, const TVector3<Real>& normal) {
+		Normal = normal;
+		Constant = -(point*normal);
+	}
+	TPlane(const TVector3<Real>& normal, const Real& constant) {
+		Normal = normal;
+		Constant = constant;
+	}
+	TPlane(const TVector3<Real>& p0, const TVector3<Real>& p1, const TVector3<Real>& p2) {
+		TVector3<Real> e0 = p1-p0;
+		TVector3<Real> e1 = p2-p0;
+		Normal = (e0%e1);
+		Normal.Normalized();
+		Constant = -(p0*Normal);
+	} 
+	TPlane(TPlane& from) {
+		Normal = from.Normal;
+		Constant = from.Constant;
+	}
+	TVector3<Real> Normal;
+	Real Constant;
+	
+	Real DistanceTo(const TVector3<Real>& point) const {
+		TVector3<Real> w = Direction.operator%(point-Point);
+		return w.Magnitude();
+	}
+private:
+};
+
+#endif
