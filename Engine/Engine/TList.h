ce57f5c49d13493229167ab2413d7b1425fd93c6
Quicksort <Quicksort@302f0312-3c5d-0d4b-b714-136c201f073e>
2012-10-12 02:02:52 +0000

    git-svn-id: https://darkengines.com/svn/darkengines/GameDev@266 302f0312-3c5d-0d4b-b714-136c201f073e

--- Engine/Engine/TList.h
+++ Engine/Engine/TList.h
@@ -4,44 +4,99 @@
 #include "System.h"
 
 template<class T>
-class TList {
+class TListItem {
 private:
 	const T* item;
-	TList* next;
+	TListItem* next;
 public:
-	TList() {}
-	TList(const T* rtItem, TList* pkNext) {
+	TListItem() {}
+	TListItem(const T* rtItem, TListItem* pkNext) {
 		item = rtItem;
 		next = pkNext;
 	}
-	~TList() {
+	~TListItem() {
 		delete item;
 	}
 	void SetItem(const T& rtItem) {
 		item = &rtItem;
 	}
-	void SetNext(TList* pkNext) {
+	void SetNext(TListItem* pkNext) {
 		next = pkNext;
 	}
 	const T& GetItem() const {
 		return *item;
 	}
-	TList* GetNext() const {
+	TListItem* GetNext() const {
 		return next;
 	}
-	TList* Next() const {
+	TListItem* Next() const {
 		return next;
 	}
-	int GetQuantity() {
+	int Length() {
 		int i = 0;
-		TList<T>* e = this;
+		TListItem<T>* e = this;
 		while (e) {
 			i++;
 			e = e->Next();
 		}
 		return i;
 	}
+
 private:
+};
+
+template<class T> class TList {
+private:
+
+public:
+	TList() {
+		_head = 0;
+		_cursor = 0;
+		_length = 0;
+	}
+	TList(const TList& from) {
+
+	}
+	~TList() {
+
+	}
+	void AppendHead(const T& item) {
+		TListItem<T>* item = new TListItem<T>(rtItem, 0);
+		if (_head) {
+			item->SetNext(_head);
+		}
+		_head = item;
+	}
+	void AppendTail(const T& item) {
+		TListItem<T>* item = new TListItem<T>(rtItem, 0);
+
+	}
+	TListItem<T>* Tail() {
+		TListItem<T>* current = _head;
+		while (current->GetNext()) {
+			current = current->GetNext();
+		}
+		return current;
+	}
+	TListItem<T>* Head() {
+		return _head;
+	}
+	void Remove(const T& item) {
+
+	}
+
+	void Insert(const T& item, const T& previous) {
+
+	}
+
+	const T& GetFirst() {
+		_cursor = _head;
+		return _cursor->GetItem();
+	}
+private:
+	TListItem<T>* _cursor;
+	TListItem<T>* _head;
+	unsigned int _length;
 };
 
 #endif
